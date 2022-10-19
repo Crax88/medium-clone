755c3477d626be2eff6140e3e1c1a47133cac224
Crax <ntrafilkin88@gmail.com>
2022-10-20 00:08:56 +0300

    Adds bootstrap function

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,16 @@
-console.log("Main");
+import { Container } from 'inversify';
+import { App } from './app';
+export interface IBootsrapReturn {
+	appContainer: Container;
+	app: App;
+}
+async function bootstrap(): Promise<IBootsrapReturn> {
+	const appContainer = new Container();
+	appContainer.bind<App>('APP').to(App).inSingletonScope();
+	const app = appContainer.get<App>('APP');
+	await app.init();
+
+	return { app, appContainer };
+}
+
+export const boot = bootstrap();

f5ba5997ef15875210c9ed28d63dfecc0c63ff04
Crax88 <45531495+Crax88@users.noreply.github.com>
2022-10-20 13:45:58 +0300

    Adds validation middleware (#2)

---
+++ backend/src/common/validation.middleware.ts
@@ -0,0 +1,29 @@
+import { ClassConstructor, plainToClass } from 'class-transformer';
+import { validate, ValidationError } from 'class-validator';
+import { Request, Response, NextFunction } from 'express';
+import { MiddlewareInterface } from './types/middleware.interface';
+import { ValidationErrorsInterface } from './types/validationErrors.interface';
+
+export class ValidationMiddleware implements MiddlewareInterface {
+	constructor(private classToValidate: ClassConstructor<object>) {}
+
+	async execute({ body }: Request, res: Response, next: NextFunction): Promise<void> {
+		const instance = plainToClass(this.classToValidate, body);
+		const errors = await validate(instance);
+		if (errors.length > 0) {
+			res.status(422).json({ errors: this.transformErrors(errors) });
+		} else {
+			next();
+		}
+	}
+
+	private transformErrors(errors: ValidationError[]): ValidationErrorsInterface {
+		return errors.reduce<ValidationErrorsInterface>((acc, cur) => {
+			acc[cur.property] = [];
+			for (const key in cur?.constraints) {
+				acc[cur.property].push(cur.constraints[key]);
+			}
+			return acc;
+		}, {});
+	}
+}

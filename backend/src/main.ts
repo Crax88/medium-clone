0d2be9287b04690fee53f639684df44c6e9ec135
Crax <ntrafilkin88@gmail.com>
2022-10-20 10:14:35 +0300

    Adds injection types map

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,14 @@
 import { Container } from 'inversify';
 import { App } from './app';
+import { TYPES } from './types';
 export interface IBootsrapReturn {
 	appContainer: Container;
 	app: App;
 }
 async function bootstrap(): Promise<IBootsrapReturn> {
 	const appContainer = new Container();
-	appContainer.bind<App>('APP').to(App).inSingletonScope();
-	const app = appContainer.get<App>('APP');
+	appContainer.bind<App>(TYPES.Application).to(App).inSingletonScope();
+	const app = appContainer.get<App>(TYPES.Application);
 	await app.init();
 
 	return { app, appContainer };

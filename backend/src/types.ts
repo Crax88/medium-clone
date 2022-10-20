b29663675000d9c8ed3cc2c911dd465065ed1890
Crax <ntrafilkin88@gmail.com>
2022-10-20 15:26:00 +0300

    Adds typeorm service

--- backend/src/types.ts
+++ backend/src/types.ts
@@ -3,4 +3,5 @@ export const TYPES = {
 	LoggerService: Symbol.for('LoggerService'),
 	ConfigService: Symbol.for('ConfigService'),
 	ExceptionFilter: Symbol.for('ExceptionFilter'),
+	DatabaseService: Symbol.for('DatabaseService'),
 };

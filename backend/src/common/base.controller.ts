d92a677dff776cefbbb4cc519547b0598bce5177
Crax <ntrafilkin88@gmail.com>
2022-10-20 10:52:34 +0300

    Adds base controller class

---
+++ backend/src/common/base.controller.ts
@@ -0,0 +1,47 @@
+import 'reflect-metadata';
+import { injectable } from 'inversify';
+import { Router, Response } from 'express';
+import { RouteInterface, ExpressReturnType } from './types/route.interface';
+
+@injectable()
+export abstract class BaseController {
+	private readonly _router: Router;
+	protected abstract _pathPrefix: string;
+
+	constructor() {
+		this._router = Router();
+	}
+
+	get router(): Router {
+		return this._router;
+	}
+
+	get pathPrefix(): string {
+		return this._pathPrefix;
+	}
+
+	protected send<T>(res: Response, code: number, data: T): ExpressReturnType {
+		res.type('application/json');
+		res.setDefaultEncoding('utf-8');
+		return res.status(code).json(data);
+	}
+
+	protected ok<T>(res: Response, data: T): ExpressReturnType {
+		return this.send<T>(res, 200, data);
+	}
+
+	protected created<T>(res: Response, data: T): ExpressReturnType {
+		return this.send<T>(res, 201, data);
+	}
+
+	protected bindRoutes(routes: RouteInterface[]): void {
+		for (const route of routes) {
+			const routePath = `${this._pathPrefix}${route.path}`.replace(/\/$/, '');
+			console.log(`[${route.method}] ${routePath}`);
+			const middleware = route?.middlewares?.map((m) => m.execute.bind(m));
+			const handler = route.handler.bind(this);
+			const pipeline = middleware ? [...middleware, handler] : handler;
+			this.router[route.method](route.path, pipeline);
+		}
+	}
+}

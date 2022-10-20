e451fb0b679fc6abbbd3507f0e4600d3c247797e
Crax <ntrafilkin88@gmail.com>
2022-10-20 11:18:27 +0300

    Adds logger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,7 @@
 import { Container } from 'inversify';
 import { App } from './app';
+import { LoggerInterface } from './common/types/logger.interface';
+import { LoggerService } from './shared/logger/logger.service';
 import { TYPES } from './types';
 export interface IBootsrapReturn {
 	appContainer: Container;
@@ -8,6 +10,7 @@ export interface IBootsrapReturn {
 async function bootstrap(): Promise<IBootsrapReturn> {
 	const appContainer = new Container();
 	appContainer.bind<App>(TYPES.Application).to(App).inSingletonScope();
+	appContainer.bind<LoggerInterface>(TYPES.LoggerService).to(LoggerService).inSingletonScope();
 	const app = appContainer.get<App>(TYPES.Application);
 	await app.init();
 

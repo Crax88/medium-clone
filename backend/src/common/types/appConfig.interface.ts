e5ac54f46c79c26b069424004f3f1dc65a6d31b0
Crax <ntrafilkin88@gmail.com>
2022-10-24 17:17:40 +0300

    Define AppConfigInterface

--- backend/src/common/types/appConfig.interface.ts
+++ backend/src/common/types/appConfig.interface.ts
@@ -1,3 +1,11 @@
-export interface AppConfigInterface extends Record<string, string> {
+export interface AppConfigInterface {
 	PORT: string;
+	NODE_ENV: string;
+	DB_HOST: string;
+	DB_PORT: string;
+	DB_NAME: string;
+	DB_USER: string;
+	DB_PASSWORD: string;
 }
+
+export type AppConfigKey = keyof AppConfigInterface;

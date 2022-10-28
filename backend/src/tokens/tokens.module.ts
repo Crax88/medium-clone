338db671c76f432ed515cdf54475fb48af9be036
Crax88 <45531495+Crax88@users.noreply.github.com>
2022-10-28 14:25:19 +0300

    Articles (#13) (#15)

--- backend/src/tokens/tokens.module.ts
+++ backend/src/tokens/tokens.module.ts
@@ -1,11 +1,8 @@
 import { ContainerModule, interfaces } from 'inversify';
-import { TokensRepository } from './tokens.repository';
 import { TokensService } from './tokens.service';
-import { TokensRepositoryInterface } from './types/tokensRepository.interface';
 import { TokensServiceInterface } from './types/tokensService.interface';
 import { TYPES } from '../types';
 
 export const TokensModule = new ContainerModule((bind: interfaces.Bind) => {
-	bind<TokensRepositoryInterface>(TYPES.TokenRepository).to(TokensRepository).inSingletonScope();
 	bind<TokensServiceInterface>(TYPES.TokenService).to(TokensService).inSingletonScope();
 });
--- backend/src/users/types/usersRepository.interface.ts
+++
@@ -1,9 +0,0 @@
-import { User } from '../user.entity';
-import { UserRegisterDto } from './userRegister.dto';
-import { UserUpdateDto } from './userUpdate.dto';
-
-export interface UsersRepositoryInterface {
-	createUser: (dto: UserRegisterDto) => Promise<User>;
-	findUser: (query: { id?: number; username?: string; email?: string }) => Promise<User | null>;
-	updateUser: (id: number, dto: UserUpdateDto) => Promise<User | null>;
-}

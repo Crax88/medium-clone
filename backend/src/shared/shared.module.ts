18bed5f4c714ba26c70134758b9eaa8a0f8e26d4
Crax88 <45531495+Crax88@users.noreply.github.com>
2022-10-28 14:25:19 +0300

    Articles (#13) (#15)

--- backend/src/shared/shared.module.ts
+++ backend/src/shared/shared.module.ts
@@ -1,13 +1,16 @@
 import { ContainerModule, interfaces } from 'inversify';
+import { TypeormService } from './services/typeorm.service';
 import { ConfigInterface } from '../common/types/config.interface';
 import { LoggerInterface } from '../common/types/logger.interface';
-import { TYPES } from '../types';
 import { ConfigService } from './services/config.service';
 import { LoggerService } from './services/logger.service';
-import { TypeormService } from './services/typeorm.service';
+import { QueryHelperService } from './services/queryHelper.service';
+import { QueryHelperInterface } from './types/queryHelper.interface';
+import { TYPES } from '../types';
 
 export const SharedModule = new ContainerModule((bind: interfaces.Bind) => {
 	bind<LoggerInterface>(TYPES.LoggerService).to(LoggerService).inSingletonScope();
 	bind<ConfigInterface>(TYPES.ConfigService).to(ConfigService).inSingletonScope();
 	bind<TypeormService>(TYPES.DatabaseService).to(TypeormService).inSingletonScope();
+	bind<QueryHelperInterface>(TYPES.QueryHelper).to(QueryHelperService).inSingletonScope();
 });

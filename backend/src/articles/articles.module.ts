6909c22a1359c9e222df2f33fce0b63b16151ad4
Crax <ntrafilkin88@gmail.com>
2022-10-31 20:56:26 +0300

    Article and tags repository

--- backend/src/articles/articles.module.ts
+++ backend/src/articles/articles.module.ts
@@ -1,8 +1,10 @@
 import { ContainerModule, interfaces } from 'inversify';
 import { ArticlesContoller } from './articles.controller';
 import { ArticlesService } from './articles.service';
+import { ArticlesRepository } from './articles.repository';
 import { ArticlesControllerInterface } from './types/articlesController.interface';
 import { ArticlesServiceInterface } from './types/articlesService.interface';
+import { ArticlesRepositoryInterface } from './types/articles.repository.interface';
 import { TYPES } from '../types';
 
 export const ArticlesModule = new ContainerModule((bind: interfaces.Bind) => {
@@ -10,4 +12,7 @@ export const ArticlesModule = new ContainerModule((bind: interfaces.Bind) => {
 		.to(ArticlesContoller)
 		.inSingletonScope();
 	bind<ArticlesServiceInterface>(TYPES.ArticlesService).to(ArticlesService).inSingletonScope();
+	bind<ArticlesRepositoryInterface>(TYPES.ArticlesRepository)
+		.to(ArticlesRepository)
+		.inSingletonScope();
 });

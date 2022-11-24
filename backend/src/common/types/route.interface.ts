1c1514b701e57cc740998ddc92623c91bc89b91a
Crax <ntrafilkin88@gmail.com>
2022-11-24 20:22:36 +0300

    Remove any

--- backend/src/common/types/route.interface.ts
+++ backend/src/common/types/route.interface.ts
@@ -3,7 +3,7 @@ import { MiddlewareInterface } from './middleware.interface';
 
 export interface RouteInterface {
 	path: string;
-	handler: (req: Request<any>, res: Response, next: NextFunction) => void;
+	handler: (req: Request, res: Response, next: NextFunction) => void;
 	method: keyof Pick<Router, 'get' | 'post' | 'delete' | 'patch' | 'put'>;
 	middlewares?: MiddlewareInterface[];
 }

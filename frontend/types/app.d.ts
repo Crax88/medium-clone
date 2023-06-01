08d1324b42bfad3f8ba45f79530626f7fc2f1307
Crax <ntrafilkin88@gmail.com>
2023-06-01 21:47:58 +0300

    More types

--- frontend/types/app.d.ts
+++ frontend/types/app.d.ts
@@ -1,3 +1,13 @@
+declare module '*.png';
+
+declare module '*.svg' {
+	const content: string;
+	// eslint-disable-next-line import/no-default-export
+	export default content;
+}
+
+declare const __API_URL__: string;
+
 declare global {
 	/**
 	 * Custom utility types

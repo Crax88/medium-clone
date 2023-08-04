993da338c352efba8ae4bd875205c09e1a196958
Crax <ntrafilkin88@gmail.com>
2023-08-04 17:05:37 +0300

    Remove comments

--- frontend/src/app/store/index.ts
+++ frontend/src/app/store/index.ts
@@ -14,7 +14,5 @@ export function makeStore() {
 
 export const appStore = makeStore();
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof appStore.getState>;
-// // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof appStore.dispatch;

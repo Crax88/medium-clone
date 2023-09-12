532587b9250ad6cbd01a2a3aa44aad9f87338dc7
Crax <ntrafilkin88@gmail.com>
2023-09-12 23:10:13 +0300

    REmove unused comments

--- frontend/types/app.d.ts
+++ frontend/types/app.d.ts
@@ -24,9 +24,8 @@ declare global {
 
 	export type Indexed<K = string, T = unknown> = { [key: K]: T };
 
-	// eslint-disable-next-line @typescript-eslint/consistent-type-imports
 	declare type RootState = import('../src/app/store').RootState;
-	// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+
 	declare type AppDispatch = import('../src/app/store').AppDispatch;
 }
 
